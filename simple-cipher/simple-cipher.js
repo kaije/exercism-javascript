5325919998d7d3722950cf1042249b5fa700e563
Kelly Farlow <k.farlow@qut.edu.au>
2018-06-12 18:24:29 +1000

    add code to ensure substition cipher can wrap on decode (step 2)

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -19,7 +19,12 @@ class Cipher {
     from the index of the encoded letter, and return the letter at the resulting index */
     return text
       .split('')
-      .map((letter, index) => this.alphabet[this.alphabet.indexOf(letter) - this.alphabet.indexOf(this.keyArray[index])])
+      .map((letter, index) => {
+        let decodeIndex = (this.alphabet.indexOf(letter) - this.alphabet.indexOf(this.keyArray[index]));
+        // if the index is negative, wrap around (e.g. -1 becomes 25)
+        decodeIndex = decodeIndex < 0 ? (26 + decodeIndex) : decodeIndex;
+        return this.alphabet[decodeIndex];
+      })
       .join('');
   }
 }
