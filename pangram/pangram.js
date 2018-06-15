199cca3632a7f2e21910ca5e97135f9fa28f1438
Kelly Farlow <k.farlow@qut.edu.au>
2018-06-15 18:30:52 +1000

    implement isPangram

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,13 +1,26 @@
 class Pangram {
   
   constructor(testString) {
-    this.testString = testString;
+    this.testString = testString? testString.toLowerCase() : testString;
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   }
 
   isPangram() {
-    switch (this.testString) {
-      case '':
-        return false;
+    let letterTally = new Map(this.alphabet.map( letter => [letter, 0]));
+    if (!this.testString) {
+      return false;
+    } else {
+      let stringArr = this.testString.split('');
+      stringArr.forEach(element => {
+        this.alphabet.includes(element) ? letterTally.set(element, letterTally.get(element) + 1) : null;
+      });
+      let thing = true;
+      letterTally.forEach((count, letter, map) => {
+        if (count === 0) {
+          thing = false;
+        }
+      });
+      return thing;
     }
   }
 }
