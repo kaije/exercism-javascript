6ecd956485c7c0a105a73a236b236aeee94a0e85
Kelly Farlow <kelly.farlow@gmail.com>
2018-06-16 00:01:52 +1000

    Change to a simpler implementation based on Another-kev's solution
    Now uses a) a regex to strip out any char that is not a lower case letter; b) a Set to capture the letters present in the given string (i.e. elements in Set are unique); and c) spread syntax to create the Set from the (iterable) stripped string.
    
    The program then simply checks that the Set has 26 elements - i.e. all 26 distinct letters as elements.

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,28 +1,19 @@
 class Pangram {
-  
   constructor(testString) {
-    this.testString = testString? testString.toLowerCase() : testString;
-    this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    this.string = testString;
   }
 
   isPangram() {
-    let letterTally = new Map(this.alphabet.map( letter => [letter, 0]));
-    if (!this.testString) {
-      return false;
-    } else {
-      let stringArr = this.testString.split('');
-      stringArr.forEach(element => {
-        this.alphabet.includes(element) ? letterTally.set(element, letterTally.get(element) + 1) : null;
-      });
-      let verdict = true;
-      letterTally.forEach((count, letter, map) => {
-        if (count === 0) {
-          verdict = false;
-        }
-      });
-      return verdict;
-    }
+    const strippedString =
+      this.string
+        .toLowerCase()
+        .replace(/[^a-z]/g, '');
+
+    const letterSet = new Set([...strippedString]);
+
+    return letterSet.size === 26;
   }
+
 }
 
 export default Pangram;
