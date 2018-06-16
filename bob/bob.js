ca2952b3dabbd21177ef47f49e38d9814568f90b
Kelly Farlow <kelly.farlow@gmail.com>
2018-06-16 11:03:02 +1000

    First (clunky) iteration

--- bob/bob.js
+++ bob/bob.js
@@ -5,10 +5,18 @@
 //
 
 class Bob {
-  hey(message) {
-    //
-    // YOUR CODE GOES HERE
-    //
+  hey(message) {    
+    if (!message.trim()) {
+      return 'Fine. Be that way!';
+    } else if (/[a-zA-Z]/.test(message) && message === message.toUpperCase() && /\?$/.test(message)) {
+      return 'Calm down, I know what I\'m doing!';    
+    } else if (/[a-zA-Z]/.test(message) && message === message.toUpperCase()) {
+      return 'Whoa, chill out!';
+    } else if (/\?$/.test(message)) {
+      return 'Sure.';
+    } else {
+      return 'Whatever.';
+    }
   }
 }
 
