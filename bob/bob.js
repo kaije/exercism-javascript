d189aec9663c0cafad647c4661e82f285c2a4225
Kelly Farlow <kelly.farlow@gmail.com>
2018-06-16 11:13:13 +1000

    Refactoring
    Renamed method to hasAtLeastOneLetter for consistency.
    
    Added isAQuestion method.

--- bob/bob.js
+++ bob/bob.js
@@ -2,23 +2,27 @@ class Bob {
   hey(message) {    
     if (!message.trim()) {
       return 'Fine. Be that way!';
-    } else if (this.atLeastOneLetter(message) && this.isAllCaps(message) && /\?$/.test(message)) {
+    } else if (this.hasAtLeastOneLetter(message) && this.isAllCaps(message) && this.isAQuestion(message)) {
       return 'Calm down, I know what I\'m doing!';    
-    } else if (this.atLeastOneLetter(message) && this.isAllCaps(message)) {
+    } else if (this.hasAtLeastOneLetter(message) && this.isAllCaps(message)) {
       return 'Whoa, chill out!';
-    } else if (/\?$/.test(message)) {
+    } else if (this.isAQuestion(message)) {
       return 'Sure.';
     } else {
       return 'Whatever.';
     }
   }
 
-  atLeastOneLetter(message) {
+  hasAtLeastOneLetter(message) {
     return /[a-zA-Z]/.test(message);
   }
 
   isAllCaps(message) {
     return message === message.toUpperCase();
+  }
+
+  isAQuestion(message) {
+    return /\?$/.test(message);
   }
 }
 
