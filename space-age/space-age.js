7ac492669bf66dac3a1baeb031a0c2d4724f1700
Kelly Farlow <k.farlow@qut.edu.au>
2018-06-19 09:45:48 +1000

    Refactor to generate onPlanet functions dynamically

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,60 +1,26 @@
 const secondsPerEarthYear = 31557600;
 
-const orbitalPeriodsInEarthYears = {
-  Mercury: 0.2408467,
-  Venus: 0.61519726,
-  Mars: 1.8808158,
-  Jupiter: 11.862615,
-  Saturn: 29.447498,
-  Uranus: 84.016846,
-  Neptune: 164.79132
+const secondsPerYear = {
+  Earth: secondsPerEarthYear,
+  Mercury: secondsPerEarthYear * 0.2408467,
+  Venus: secondsPerEarthYear * 0.61519726,
+  Mars: secondsPerEarthYear * 1.8808158,
+  Jupiter: secondsPerEarthYear * 11.862615,
+  Saturn: secondsPerEarthYear * 29.447498,
+  Uranus: secondsPerEarthYear * 84.016846,
+  Neptune: secondsPerEarthYear * 164.79132
 };
 
 class SpaceAge {
   constructor(seconds) {
     this.seconds = seconds;
-    this.ageInEarthYears = seconds / secondsPerEarthYear;
-  }
 
-  calculateYears(planet) {
-    return this.formatResult(this.ageInEarthYears / orbitalPeriodsInEarthYears[planet]);
+    for (const planet in secondsPerYear) {
+      this[`on${planet}`] = () => {
+        return Number((this.seconds / secondsPerYear[planet]).toFixed(2));
+      }
+    }
   }
-
-  formatResult(years) {
-    return Number.parseFloat(Number.parseFloat(years).toFixed(2));
-  }
-
-  onEarth() {
-    return this.formatResult(this.ageInEarthYears);
-  }
-
-  onMercury() {
-    return this.calculateYears('Mercury');
-  }
-
-  onVenus() {
-    return this.calculateYears('Venus');    
-  }
-
-  onMars() {
-    return this.calculateYears('Mars');
-  }
-
-  onJupiter() {
-    return this.calculateYears('Jupiter');
-  }
-
-  onSaturn() {
-    return this.calculateYears('Saturn');
-  }
-
-  onUranus() {
-    return this.calculateYears('Uranus');
-  }
-
-  onNeptune() {
-    return this.calculateYears('Neptune');
-  }
-}
+} 
 
 export default SpaceAge;
