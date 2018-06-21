baea7ab97eb248e1752e75f1b2d78b76261d13fc
Kelly Farlow <k.farlow@qut.edu.au>
2018-06-21 10:59:21 +1000

    Implement full solution

--- binary/binary.js
+++ binary/binary.js
@@ -1,11 +1,21 @@
-class Binary {
+export default class Binary {
   constructor(binaryString) {
     this.binaryString = binaryString;
+    this.inputValid = /^[01]+$/.test(this.binaryString);
   }
 
   toDecimal() {
-    return Number(this.binaryString);
+    return this.inputValid ? this.convert() : 0;
+  }
+
+  convert() {
+    let decimal = 0;
+    let exponent = 0;
+    
+    for (let i = this.binaryString.length - 1; i >= 0; i--) {
+      decimal = decimal + (this.binaryString.charAt(i) * 2**exponent);
+      exponent++;
+    }
+    return decimal;
   }
 }
-
-export default Binary;
