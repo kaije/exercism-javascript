7cf615f402cc356bd9372f3de88c5f2f9e1dd5f0
Kelly Farlow <k.farlow@qut.edu.au>
2018-06-22 16:38:22 +1000

    Add check for prime divisor and extra test to check non-primes are not added to factors

--- prime-factors/prime-factors.js
+++ prime-factors/prime-factors.js
@@ -5,15 +5,28 @@ export default class PrimeFactors {
     return number === 1 ? [] : this.getPrimeFactors(number);
   }
 
+  isPrime(number) {
+    let isPrime = true;
+    for (var i = 2; i <= number / 2; i++) {
+      isPrime = i%number == 0 ? false : true;
+    }
+    return isPrime;
+  }
+
   getPrimeFactors(number) {
     let divisor = 2;
     let remainder = number;
     let factors = [];
 
     while( remainder > 1 ) {
-      if ( remainder%divisor === 0 ) {
-        factors.push(divisor);
-        remainder = Math.floor(remainder/divisor);
+      console.log(`checking divisor ${divisor}`);
+      if (this.isPrime(divisor)) {
+        if ( remainder%divisor === 0 ) {        
+          factors.push(divisor);
+          remainder /= divisor;
+        } else {
+          divisor++;
+        }
       } else {
         divisor++;
       }
