082d6d37a5311bd46786e14e5179be8eed38fdd1
Kelly Farlow <kelly.farlow@gmail.com>
2018-06-23 22:17:15 +1000

    Add clunky first iteration for Matrix exercise

---
+++ matrix/matrix.js
@@ -0,0 +1,22 @@
+export default class Matrix {
+  constructor(matrix) {
+    this.rows = []
+    this.columns = []
+
+    this.rowsArray = matrix.split('\n')
+      .forEach( (row, index) => {
+        let rowArr = row.split(' ').map( value => +value);
+        this.rows[index] = rowArr;
+      });
+
+      console.log(this.rows);
+      console.log(this.columns);
+
+    this.rows.forEach( (rowArr, rowIndex) => {
+      rowArr.forEach( (element, colIndex) => {
+        this.columns[colIndex] = this.columns[colIndex] || [];
+        this.columns[colIndex].push(element);
+      });
+    });
+  }
+}
