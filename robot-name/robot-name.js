bd8fc5b4ae79b45c19022cdf0e6f73b510f8449e
Kelly Farlow <kelly.farlow@gmail.com>
2018-07-06 22:57:02 +1000

    Add partial solution for Robot Name exercise (ECMAScript)

---
+++ robot-name/robot-name.js
@@ -0,0 +1,43 @@
+let usedNames = new Set();
+
+export default class Robot {
+  constructor() {
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.setName();
+  }
+
+  reset() {
+    this.setName();
+  }
+
+  setName() {
+    this.name = this.generateName();
+  }
+
+  /* Names must be in the format of two uppercase letters followed by 
+  three digits, such as RX837 or BC811 */
+  generateName() {
+    let letters;
+    let digits;
+    let newName = 'none';
+    
+    letters = this.alphabet[this.getRandomIntInclusive(0,25)] + this.alphabet[this.getRandomIntInclusive(0,25)];
+    digits = this.getRandomIntInclusive(0,999);
+    digits = digits.toString().padStart(3, '0');
+    newName = `${letters}${digits.toString()}`;
+
+    if (usedNames.has(newName)) {
+      this.generateName();
+    } else {
+      usedNames.add(newName);
+      return newName;
+    }
+  }
+
+  getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  }
+
+}
