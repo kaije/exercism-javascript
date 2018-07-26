2fc7d52cb47b5daeed653dde54fdb7d22b0fb566
Kelly Farlow <k.farlow@qut.edu.au>
2018-07-26 15:08:09 +1000

    Start implementing solution for Protein Translation exercise - 8/14 tests passing

---
+++ protein-translation/protein-translation.js
@@ -0,0 +1,29 @@
+export default function translate(rnaSequence) {
+  const translations = {
+    AUG: 'Methionine',
+    UUU: 'Phenylalanine',
+    UUC: 'Phenylalanine',
+    UUA: 'Leucine',
+    UUG: 'Leucine',
+    UCU: 'Serine',
+    UCC: 'Serine',
+    UCA: 'Serine',
+    UCG: 'Serine',
+    UAU: 'Tyrosine',
+    UAC: 'Tyrosine',
+    UGU: 'Cysteine',
+    UGC: 'Cysteine',
+    UGG: 'Tryptophan',
+    UAA: 'STOP',
+    UAG: 'STOP',
+    UGA: 'STOP'
+  }
+
+  let protein = [];
+
+  let codons = rnaSequence ? rnaSequence.match(/.{1,3}/g) : [];
+
+  codons.forEach( codon => protein.push(translations[codon]) );
+
+  return protein;
+}
