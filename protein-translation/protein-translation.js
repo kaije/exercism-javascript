d765a3443f1ea2339a691ac97f9d56b891020c50
Kelly Farlow <k.farlow@qut.edu.au>
2018-07-27 11:17:35 +1000

    Complete clunky first iteration solution for Protein Translation exercise - all tests now passing

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -19,11 +19,31 @@ export default function translate(rnaSequence) {
     UGA: 'STOP'
   }
 
+  const aminoAcids = {
+    Methionine: ['AUG'],
+    Phenylalanine: ['UUU','UUC'],
+    Leucine: ['UUA','UUG'],
+    Serine: ['UCU','UCC','UCA','UCG'],
+    Tyrosine: ['UAU','UAC'],
+    Cysteine: ['UGU','UGC'],
+    Tryptophan: ['UGG'],
+    STOP: ['UAA','UAG','UGA']
+  }
+
   let protein = [];
 
   let codons = rnaSequence ? rnaSequence.match(/.{1,3}/g) : [];
 
-  codons.forEach( codon => protein.push(translations[codon]) );
+  //codons.forEach( codon => protein.push(translations[codon]) );
+  for (let i=0; i < codons.length; i++) {
+    if (aminoAcids.STOP.includes(codons[i])) {
+      break;
+    }
+    if (!Object.keys(translations).includes(codons[i])) {
+      throw new Error('Invalid codon');
+    }
+    protein.push(translations[codons[i]]);
+  }
 
   return protein;
 }
