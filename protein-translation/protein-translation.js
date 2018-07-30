edf899e384ee0f5ded346fe1d9f1110b635ecc7d
Kelly Farlow <kelly.farlow@gmail.com>
2018-07-30 22:46:16 +1000

    Refactor

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -19,28 +19,16 @@ export default function translate(rnaSequence) {
     UGA: 'STOP'
   }
 
-  const aminoAcids = {
-    Methionine: ['AUG'],
-    Phenylalanine: ['UUU','UUC'],
-    Leucine: ['UUA','UUG'],
-    Serine: ['UCU','UCC','UCA','UCG'],
-    Tyrosine: ['UAU','UAC'],
-    Cysteine: ['UGU','UGC'],
-    Tryptophan: ['UGG'],
-    STOP: ['UAA','UAG','UGA']
-  }
-
   let protein = [];
 
   let codons = rnaSequence ? rnaSequence.match(/.{1,3}/g) : [];
 
-  //codons.forEach( codon => protein.push(translations[codon]) );
   for (let i=0; i < codons.length; i++) {
-    if (aminoAcids.STOP.includes(codons[i])) {
-      break;
-    }
     if (!Object.keys(translations).includes(codons[i])) {
       throw new Error('Invalid codon');
+    }
+    if (translations[codons[i]]==='STOP') {
+      break;
     }
     protein.push(translations[codons[i]]);
   }
