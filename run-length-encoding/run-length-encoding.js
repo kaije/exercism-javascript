e243e0fb2fecc1eb8778f216e67f94a48eb59ea9
Kelly Farlow <k.farlow@qut.edu.au>
2018-08-02 10:57:01 +1000

    Implement decode function (all tests now passing)

--- run-length-encoding/run-length-encoding.js
+++ run-length-encoding/run-length-encoding.js
@@ -1,20 +1,29 @@
 export function encode(str) {
   let encoded = '';
   if (str) {
-    let runs = str.match(/(.)\1+|./g);
-    console.log(runs);
+    const runs = str.match(/(.)\1+|./g);
     runs.forEach( run => { 
       if (run.length > 1) {     
         encoded = `${encoded}${run.length}${run.charAt(0)}`
       } else {
         encoded = `${encoded}${run}`;
       }
-      console.log(encoded);
     });
   }
   return encoded;
 }
 
 export function decode(str) {
-
+  let decoded = '';
+  if (str) {
+    const compressions = str.match(/([0-9])+[a-zA-Z ]|[a-zA-Z ]/g);
+    compressions.forEach( compression => {
+      if(/[0-9]/.test(compression)) {
+        decoded = `${decoded}${compression.match(/[a-zA-Z ]/)[0].repeat(compression.match(/[0-9]+/)[0])}`;
+      } else {
+        decoded = `${decoded}${compression}`;
+      }
+    });
+  }
+  return decoded;
 }
