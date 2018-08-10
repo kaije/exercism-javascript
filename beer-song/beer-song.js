82a0426a73404d66590fe89b501dcb643e6ae832
Kelly Farlow <k.farlow@qut.edu.au>
2018-08-10 15:07:26 +1000

    Refactor to remove duplication

--- beer-song/beer-song.js
+++ beer-song/beer-song.js
@@ -1,31 +1,37 @@
 export default class Beer {
-  constructor() {
-
-  }
 
   static verse(bottles) {
     let firstLine;
     let secondLine;
-  
+    let state = this.state(bottles);
+ 
+    firstLine = `${state.currentBottlesDesc} of beer on the wall, ${state.currentBottlesDesc.toLowerCase()} of beer.`;
+    secondLine = `${state.actionDesc}, ${state.newBottlesDesc} of beer on the wall.`;
+
+    return `${firstLine}\n${secondLine}\n`;
+  }
+
+  static state(bottles) {
+    let state = {};
+    
     switch (bottles) {
-      case 0:
-        firstLine = `No more bottles of beer on the wall, no more bottles of beer.`;
-        secondLine = `Go to the store and buy some more, 99 bottles of beer on the wall.`;
+      case 0: 
+        state.currentBottlesDesc = 'No more bottles';
+        state.actionDesc = 'Go to the store and buy some more';
+        state.newBottlesDesc = '99 bottles';
         break;
       case 1:
-        firstLine = `1 bottle of beer on the wall, 1 bottle of beer.`;
-        secondLine = `Take it down and pass it around, no more bottles of beer on the wall.`;
+        state.currentBottlesDesc = '1 bottle';
+        state.actionDesc = 'Take it down and pass it around';
+        state.newBottlesDesc = 'no more bottles';
         break;
-      case 2:
-        firstLine = `2 bottles of beer on the wall, 2 bottles of beer.`;
-        secondLine = `Take one down and pass it around, 1 bottle of beer on the wall.`;
-        break;        
       default:
-        firstLine = `${bottles} bottles of beer on the wall, ${bottles} bottles of beer.`;
-        secondLine =`Take one down and pass it around, ${bottles-1} bottles of beer on the wall.`;
+        state.currentBottlesDesc = `${bottles} bottles`;
+        state.actionDesc = 'Take one down and pass it around';
+        state.newBottlesDesc = `${bottles-1} ${bottles === 2 ? 'bottle' : 'bottles' }`;
     }
-  
-    return `${firstLine}\n${secondLine}\n`;
+
+    return state;
   }
 
   static sing(start = 99, finish = 0) {
