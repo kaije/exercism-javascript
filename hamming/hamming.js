0d30cc55bc9eb93acdd96267b0e715a1c9e59132
Kelly Farlow <kelly.farlow@gmail.com>
2018-08-29 22:21:59 +1000

    Hamming: Refactor

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -3,21 +3,19 @@ export default class Hamming {
 
   compute(strand1, strand2) {
     if (strand1.length !== strand2.length) {
-      throw new Error('DNA strands must be of equal length.');
+      throw new Error("DNA strands must be of equal length.");
     }
-    
-    if (strand1 === strand2) {
-      return 0;
-    } else {
-      let hammingDistance = 0;
-  
+
+    let hammingDistance = 0;
+
+    if (strand1 !== strand2) {
       for (let i = 0; i < strand1.length; i++) {
         if (strand1.charAt(i) !== strand2.charAt(i)) {
           hammingDistance++;
         }
       }
-
-      return hammingDistance;
     }
+
+    return hammingDistance;
   }
 }
