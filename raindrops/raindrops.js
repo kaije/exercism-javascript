78cc13b2f83a8fe23d08a9a298300d8fae2387c5
Kelly Farlow <kelly.farlow@gmail.com>
2018-08-31 17:37:25 +1000

    Raindrops: Refactor

--- raindrops/raindrops.js
+++ raindrops/raindrops.js
@@ -1,24 +1,21 @@
 export default class Raindrops {
-  constructor() {     
-    this.sounds = {
+  constructor() { }
+
+  convert(num) {
+    const sounds = {
       3: 'Pling',
       5: 'Plang',
       7: 'Plong'
     } 
-  }
 
-  convert(num) {
     let str = '';
 
-    for (var factor in this.sounds) {
-      str = this.check(str, num, factor);
-    }
-    return str ? str : num.toString();
-  }
-
-  check(str, num, factor) {
-    return num >= factor && num % factor === 0
-      ? str + this.sounds[factor]
+    for (var factor in sounds) {
+      str = num >= factor && num % factor === 0
+      ? str + sounds[factor]
       : str;
+    }
+
+    return str ? str : num.toString();
   }
 }
