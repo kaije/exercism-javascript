cce1fa9ebd157427fb6dce91f9ac1f53cbc63d64
Kelly Farlow <kelly.farlow@gmail.com>
2018-09-19 16:08:59 +1000

    Collatz Conjecture: Revise and complete solution - all tests now passing

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,17 +1,22 @@
 function steps(num) {
-  let result = num;
+  let n = num;
   let stepCnt = 0;
 
-  while (stepCnt <= 10 && result !== 1) {
-    console.log(`result=${result}`);
-    let temp = result / 2;
-    if (temp === 1) {
-      result = temp;
+  if (n <= 0) {
+    throw new Error('Only positive numbers are allowed');
+  }
+
+  while (n !== 1) {
+
+    // if n is even, divide n by 2
+    if (n % 2 === 0) {
+      n = n / 2;
     } else {
-      result = temp % 2 !== 0 ? (3 * result) + 1 : temp;
+      // if n is odd, multiply n by 3 and add 1
+      n = (3 * n) + 1;
     }
-    stepCnt = stepCnt + 1;
-    console.log(`stepCnt=${stepCnt}`);
+
+    stepCnt++;
   }
 
   return stepCnt;
