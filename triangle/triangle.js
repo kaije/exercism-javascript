8c43fad7e215f4a57caa6f852c53a51bac3e7216
Kelly Farlow <kelly.farlow@gmail.com>
2018-10-05 17:24:10 +1000

    Refactor

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,34 +1,32 @@
 export default class Triangle {
-  constructor(a,b,c) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
+  constructor(a, b, c) {
+    this.sides = {
+      a: a,
+      b: b,
+      c: c
+    };
   }
 
   kind() {
-
     if (!this.isLegal()) {
       throw new Error();
-    }    
-
-    if (this.a === this.b && this.b === this.c) {      
-      return 'equilateral';
     }
 
-    if (this.a === this.b || this.b === this.c || this.c === this.a) {
-      return 'isosceles';
-    }
-
-    if (this.a !== this.b && this.b !== this.c && this.c !== this.a) {
-      return 'scalene';
-    }
+    return this.sides.a === this.sides.b && this.sides.b === this.sides.c
+      ? "equilateral"
+      : this.sides.a === this.sides.b ||
+        this.sides.b === this.sides.c ||
+        this.sides.c === this.sides.a
+        ? "isosceles"
+        : "scalene";
   }
 
   isLegal() {
-    if (this.a <= 0 || this.b <= 0 || this.c <= 0) {
-      return false;
-    }
-
-    return (this.a + this.b >= this.c && this.b + this.c >= this.a && this.c + this.a >= this.b);
+    return (
+      Object.values(this.sides).every(side => side > 0) &&
+      (this.sides.a + this.sides.b >= this.sides.c &&
+        this.sides.b + this.sides.c >= this.sides.a &&
+        this.sides.c + this.sides.a >= this.sides.b)
+    );
   }
 }
