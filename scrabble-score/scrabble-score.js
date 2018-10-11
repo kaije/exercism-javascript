0a39b4f5de8ab7b0b683317cf3c1e521b0cc038b
Kelly Farlow <kelly.farlow@gmail.com>
2018-10-11 15:16:19 +1000

    Scrabble Scores: Replace switch statement with object

--- scrabble-score/scrabble-score.js
+++ scrabble-score/scrabble-score.js
@@ -6,41 +6,19 @@ export default function score(word) {
 }
 
 function getLetterScore(letter) {
-  switch (letter.toUpperCase()) {
-    case "A":
-    case "E":
-    case "I":
-    case "O":
-    case "U":
-    case "L":
-    case "N":
-    case "R":
-    case "S":
-    case "T":
-      return 1;
-    case "D":
-    case "G":
-      return 2;
-    case "B":
-    case "C":
-    case "M":
-    case "P":
-      return 3;
-    case "F":
-    case "H":
-    case "V":
-    case "W":
-    case "Y":
-      return 4;
-    case "K":
-      return 5;
-    case "J":
-    case "X":
-      return 8;
-    case "Q":
-    case "Z":
-      return 10;
-    default:
-      return 0;
-  }
+  const letterScores = {
+    1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2: ["D", "G"],
+    3: ["B", "C", "M", "P"],
+    4: ["F", "H", "V", "W", "Y"],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q", "Z"]
+  };
+
+  return parseInt(
+    Object.entries(letterScores).find(([score, letters]) =>
+      letters.includes(letter.toUpperCase())
+    )[0]
+  );
 }
