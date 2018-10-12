c3aca8b9ad7974dc9425202936ea194716a30d23
Kelly Farlow <kelly.farlow@gmail.com>
2018-10-12 13:22:51 +1000

    Proverb: Refactor to use while loop

--- proverb/proverb.js
+++ proverb/proverb.js
@@ -1,18 +1,15 @@
 export default function proverb(...consequences) {
-  const final = consequences[0];
+  const first = consequences[0];
   let curr;
   let next;
-
   let proverb;
 
-  consequences.forEach((element, index) => {
-    curr = element;
-    next = index < consequences.length - 1 ? consequences[index + 1] : null;
-    if (next) {
-      proverb = proverb ? `${proverb}\n` : "";
-      proverb = `${proverb}For want of a ${curr} the ${next} was lost.`;
-    }
-  }, this);
-  proverb = `${proverb}\nAnd all for the want of a ${final}.`;
+  while (consequences.length > 1) {
+    curr = consequences.shift();
+    next = consequences[0];
+    proverb = proverb ? `${proverb}\n` : "";
+    proverb = `${proverb}For want of a ${curr} the ${next} was lost.`;
+  }
+  proverb = `${proverb}\nAnd all for the want of a ${first}.`;
   return proverb;
 }
