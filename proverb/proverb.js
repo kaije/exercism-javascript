b2bafcde5e12c48b874447571e182e7b2f2730ba
Kelly Farlow <kelly.farlow@gmail.com>
2018-10-16 13:18:46 +1000

    Proverb: Completed solution

--- proverb/proverb.js
+++ proverb/proverb.js
@@ -1,5 +1,6 @@
 export default function proverb(...consequences) {
   const first = consequences[0];
+  let qualifier;
   let curr;
   let next;
   let proverb;
@@ -7,10 +8,16 @@ export default function proverb(...consequences) {
   while (consequences.length > 1) {
     curr = consequences.shift();
     next = consequences[0];
-    proverb = `${
-      proverb ? `${proverb}\n` : ``
-    }For want of a ${curr} the ${next} was lost.`;
+    if (typeof next === "object") {
+      qualifier = next.qualifier;
+    } else {
+      proverb = `${
+        proverb ? `${proverb}\n` : ``
+      }For want of a ${curr} the ${next} was lost.`;
+    }
   }
-  proverb = `${proverb}\nAnd all for the want of a ${first}.`;
+  proverb = `${proverb}\nAnd all for the want of a${
+    qualifier ? ` ${qualifier}` : ``
+  } ${first}.`;
   return proverb;
 }
