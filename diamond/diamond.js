7db87f9908a6a6430f36d4b0152abc615abb7110
Kelly Farlow <kelly.farlow@gmail.com>
2018-10-23 18:05:49 +1000

    Diamond: WIP

--- diamond/diamond.js
+++ diamond/diamond.js
@@ -5,5 +5,27 @@ export default class Diamond {
     if (letter === 'A'){
       return `${letter}\n`;
     };
+
+    let letters = ['A','B','C','D','E','F'];
+    
+    let rows = [];
+    let index = letters.indexOf(letter);
+    let outerSpacesCnt = 0;
+    let innerSpacesCnt = index * 2 - 1;
+    
+    while (index > 0) {
+      // add a row for the target letter
+      let outerSpaces = '.'.repeat(outerSpacesCnt);
+      let innerSpaces = '.'.repeat(innerSpacesCnt);
+      let row = `${outerSpaces}${letters[index]}${innerSpaces}${letters[index]}${outerSpaces}`;
+      console.log(row);
+      rows.unshift(row);
+      rows.push(row);
+      outerSpacesCnt++;
+      innerSpacesCnt = innerSpacesCnt > 2 ? innerSpacesCnt - 2 : 0;
+      index--;
+    }
+
+    return `${rows.join('\n')}\n`;
   }
 }
