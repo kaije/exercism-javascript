e0a1f30dda606d94fe1700491c8954fd2a1b9400
Kelly Farlow <kelly.farlow@gmail.com>
2018-10-24 11:19:09 +1000

    Diamond: Extend initial solution - 2 tests now passing

--- diamond/diamond.js
+++ diamond/diamond.js
@@ -13,18 +13,31 @@ export default class Diamond {
     let outerSpacesCnt = 0;
     let innerSpacesCnt = index * 2 - 1;
     
+    let row = '';
+    let outerSpaces = '';
+    let innerSpaces = '';
+
     while (index > 0) {
       // add a row for the target letter
-      let outerSpaces = '.'.repeat(outerSpacesCnt);
-      let innerSpaces = '.'.repeat(innerSpacesCnt);
-      let row = `${outerSpaces}${letters[index]}${innerSpaces}${letters[index]}${outerSpaces}`;
+      outerSpaces = ' '.repeat(outerSpacesCnt);
+      innerSpaces = ' '.repeat(innerSpacesCnt);
+      row = `${outerSpaces}${letters[index]}${innerSpaces}${letters[index]}${outerSpaces}`;
       console.log(row);
-      rows.unshift(row);
+      if (rows.length > 0) {
+        rows.unshift(row);
+      }
       rows.push(row);
       outerSpacesCnt++;
       innerSpacesCnt = innerSpacesCnt > 2 ? innerSpacesCnt - 2 : 0;
       index--;
     }
+
+    // add first and last rows (A)
+    outerSpaces = ' '.repeat(outerSpacesCnt);
+    row = `${outerSpaces}${letters[index]}${outerSpaces}`;
+    console.log(row);
+    rows.unshift(row);
+    rows.push(row);
 
     return `${rows.join('\n')}\n`;
   }
