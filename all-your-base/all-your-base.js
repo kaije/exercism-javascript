02b12df219bfa7bc5e32e49b8112c69c08211ef2
Kelly Farlow <kelly.farlow@gmail.com>
2018-11-07 11:44:23 +1000

    All Your Base: Completed first iteration of solution - all tests now passing

--- all-your-base/all-your-base.js
+++ all-your-base/all-your-base.js
@@ -1,17 +1,50 @@
 export default class Converter {
   constructor() {}
 
-  convert(digits, base, newBase) {
+  convert(digits, inputBase, outputBase) {
+
+    if (inputBase === 1 || inputBase === 0 || inputBase < 0 || !inputBase || !Number.isInteger(inputBase)) {
+      throw new Error('Wrong input base');    
+    }
+
+    if (outputBase === 1 || outputBase ===0 || outputBase < 0 || !outputBase || !Number.isInteger(outputBase)) {
+      throw new Error('Wrong output base');    
+    }
+    
+    if (digits.filter( digit => digit >= inputBase).length > 0) {
+      throw new Error('Input has wrong format');
+    }
+
+    if (digits.length === 0) {
+      throw new Error('Input has wrong format');
+    }
+
+    if (digits.length === 1 && digits[0] === 0) {
+      return [0];
+    }
+
+    if (digits.length > 1 && digits.filter( digit => digit !== 0).length === 0) {
+      throw new Error('Input has wrong format');    
+    }
+
+    if (digits.length > 1 && digits[0] === 0) {
+      throw new Error('Input has wrong format');    
+    }    
+
+    if (digits.filter( digit => digit < 0).length > 0) {
+      throw new Error('Input has wrong format');    
+    }
+
     let decimal = digits
       .reverse()
-      .map((digit, index) => digit * base ** index)
+      .map((digit, index) => digit * inputBase ** index)
       .reduce((sum, digit) => sum + digit);
 
     let baseArray = [];
     let curr;
     let exponent = 0;
     do {
-      curr = newBase ** exponent;
+      curr = outputBase ** exponent;
       if (decimal >= curr) {
         baseArray.push(curr);
       }
--- all-your-base/all-your-base.spec.js
+++ all-your-base/all-your-base.spec.js
@@ -36,101 +36,101 @@ describe('Converter', () => {
     expect(converter.convert([3, 46, 60], 97, 73)).toEqual([6, 10, 45]);
   });
 
-  xtest('empty list', () => {
+  test('empty list', () => {
     expect(() => {
       converter.convert([], 2, 10);
     }).toThrow(new Error('Input has wrong format'));
   });
 
-  xtest('single zero', () => {
+  test('single zero', () => {
     expect(converter.convert([0], 10, 2)).toEqual([0]);
   });
 
-  xtest('multiple zeros', () => {
+  test('multiple zeros', () => {
     expect(() => {
       converter.convert([0, 0, 0], 10, 2);
     }).toThrow(new Error('Input has wrong format'));
   });
 
-  xtest('leading zeros', () => {
+  test('leading zeros', () => {
     expect(() => {
       converter.convert([0, 6, 0], 7, 10);
     }).toThrow(new Error('Input has wrong format'));
   });
 
-  xtest('negative digit', () => {
+  test('negative digit', () => {
     expect(() => {
       converter.convert([1, -1, 1, 0, 1, 0], 2, 10);
     }).toThrow(new Error('Input has wrong format'));
   });
 
-  xtest('invalid positive digit', () => {
+  test('invalid positive digit', () => {
     expect(() => {
       converter.convert([1, 2, 1, 0, 1, 0], 2, 10);
     }).toThrow(new Error('Input has wrong format'));
   });
 
-  xtest('first base is one', () => {
+  test('first base is one', () => {
     expect(() => {
       converter.convert([], 1, 10);
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('second base is one', () => {
+  test('second base is one', () => {
     expect(() => {
       converter.convert([1, 0, 1, 0, 1, 0], 2, 1);
     }).toThrow(new Error('Wrong output base'));
   });
 
-  xtest('first base is zero', () => {
+  test('first base is zero', () => {
     expect(() => {
       converter.convert([], 0, 10);
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('second base is zero', () => {
+  test('second base is zero', () => {
     expect(() => {
       converter.convert([7], 10, 0);
     }).toThrow(new Error('Wrong output base'));
   });
 
-  xtest('first base is negative', () => {
+  test('first base is negative', () => {
     expect(() => {
       converter.convert([1], -2, 10);
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('second base is negative', () => {
+  test('second base is negative', () => {
     expect(() => {
       converter.convert([1], 2, -7);
     }).toThrow(new Error('Wrong output base'));
   });
 
-  xtest('both bases are negative', () => {
+  test('both bases are negative', () => {
     expect(() => {
       converter.convert([1], -2, -7);
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('missing input base throws an error', () => {
+  test('missing input base throws an error', () => {
     expect(() => {
       converter.convert([0]);
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('wrong input_base base not integer', () => {
+  test('wrong input_base base not integer', () => {
     expect(() => {
       converter.convert([0], 2.5);
     }).toThrow(new Error('Wrong input base'));
   });
 
-  xtest('missing output base throws an error', () => {
+  test('missing output base throws an error', () => {
     expect(() => {
       converter.convert([0], 2);
     }).toThrow(new Error('Wrong output base'));
   });
 
-  xtest('wrong output_base base not integer', () => {
+  test('wrong output_base base not integer', () => {
     expect(() => {
       converter.convert([0], 3, 2.5);
     }).toThrow(new Error('Wrong output base'));
