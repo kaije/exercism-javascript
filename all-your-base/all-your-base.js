54f4abf9c29183bceea0d3b75dbfbd869e6b93e0
Kelly Farlow <kelly.farlow@gmail.com>
2018-11-08 14:13:39 +1000

    All Your Base: Refactor for readability

--- all-your-base/all-your-base.js
+++ all-your-base/all-your-base.js
@@ -2,37 +2,10 @@ export default class Converter {
   constructor() {}
 
   convert(digits, inputBase, outputBase) {
+    this.validate(digits, inputBase, outputBase);
 
-    if (inputBase === 1 || inputBase === 0 || inputBase < 0 || !inputBase || !Number.isInteger(inputBase)) {
-      throw new Error('Wrong input base');    
-    }
-
-    if (outputBase === 1 || outputBase ===0 || outputBase < 0 || !outputBase || !Number.isInteger(outputBase)) {
-      throw new Error('Wrong output base');    
-    }
-    
-    if (digits.filter( digit => digit >= inputBase).length > 0) {
-      throw new Error('Input has wrong format');
-    }
-
-    if (digits.length === 0) {
-      throw new Error('Input has wrong format');
-    }
-
-    if (digits.length === 1 && digits[0] === 0) {
+    if (digits.join('') === '0') {
       return [0];
-    }
-
-    if (digits.length > 1 && digits.filter( digit => digit !== 0).length === 0) {
-      throw new Error('Input has wrong format');    
-    }
-
-    if (digits.length > 1 && digits[0] === 0) {
-      throw new Error('Input has wrong format');    
-    }    
-
-    if (digits.filter( digit => digit < 0).length > 0) {
-      throw new Error('Input has wrong format');    
     }
 
     let decimal = digits
@@ -40,27 +13,69 @@ export default class Converter {
       .map((digit, index) => digit * inputBase ** index)
       .reduce((sum, digit) => sum + digit);
 
-    let baseArray = [];
-    let curr;
+    let outputArray = [];
+    let current;
     let exponent = 0;
     do {
-      curr = outputBase ** exponent;
-      if (decimal >= curr) {
-        baseArray.push(curr);
+      current = outputBase ** exponent;
+      if (decimal >= current) {
+        outputArray.push(current);
       }
       exponent++;
-    } while (decimal >= curr);
+    } while (decimal >= current);
 
-    baseArray.reverse();
+    outputArray.reverse();
 
     let newVal;
     let remainder = decimal;
-    let result = baseArray.map(val => {
+    let result = outputArray.map(val => {
       newVal = Math.floor(remainder / val);
       remainder = remainder % val;
       return newVal;
     });
 
     return result;
+  }
+
+  validate(digits, inputBase, outputBase) {
+    this.validateInputBase(inputBase);
+    this.validateOutputBase(outputBase);
+    this.validateInput(digits, inputBase);
+  }
+
+  validateInputBase(inputBase) {
+    if (
+      inputBase === 1 ||
+      inputBase === 0 ||
+      inputBase < 0 ||
+      !inputBase ||
+      !Number.isInteger(inputBase)
+    ) {
+      throw new Error("Wrong input base");
+    }
+  }
+
+  validateOutputBase(outputBase) {
+    if (
+      outputBase === 1 ||
+      outputBase === 0 ||
+      outputBase < 0 ||
+      !outputBase ||
+      !Number.isInteger(outputBase)
+    ) {
+      throw new Error("Wrong output base");
+    }
+  }
+
+  validateInput(digits, inputBase) {
+    if (
+      digits.filter(digit => digit >= inputBase).length > 0 ||
+      digits.length === 0 ||
+      (digits.length > 1 && digits.filter(digit => digit !== 0).length === 0) ||
+      (digits.length > 1 && digits[0] === 0) ||
+      digits.filter(digit => digit < 0).length > 0
+    ) {
+      throw new Error("Input has wrong format");
+    }
   }
 }
