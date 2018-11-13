e306951c44396de7bcaf087d90eb44d19e2e3126
Kelly Farlow <kelly.farlow@gmail.com>
2018-11-13 10:04:46 +1000

    Grains: Extend solution - 7 of 8 tests now passing

--- grains/grains.js
+++ grains/grains.js
@@ -3,6 +3,6 @@ import BigInt from './lib/big-integer';
 export default class Grains {
   constructor() {}
   square(id) {
-    return `${BigInt(2 ** (id - 1))}`;
+    return BigInt(2).pow(id-1).toString();
   }
 }
