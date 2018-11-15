e947dbbc6dc8a06b7d0bf19becc1b9092808541b
Kelly Farlow <kelly.farlow@gmail.com>
2018-11-15 11:13:30 +1000

    Perfect Numbers: Extended solution - all tests now passing

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -2,6 +2,23 @@ function classify(num) {
   if (num <= 0) {
     throw new Error("Classification is only possible for natural numbers.");
   }
+  let factors = [];
+  let candidate = 1;
+  while (candidate < num) {
+    if (num % candidate === 0) {
+      factors.push(candidate);
+    }
+    candidate++;
+  }
+  let sum =
+    factors.length > 0 ? factors.reduce((sum, current) => sum + current) : 0;
+  if (sum === num) {
+    return "perfect";
+  } else if (sum > num) {
+    return "abundant";
+  } else {
+    return "deficient";
+  }
 }
 
 export { classify };
