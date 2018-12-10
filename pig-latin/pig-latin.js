78e2f9a174ba9ce7e8c1d0ec1c671727ed2a92e0
Kelly Farlow <kelly.farlow@gmail.com>
2018-12-10 17:33:27 +1000

    Pig Latin: WIP - 1 of 11 tests passing

--- pig-latin/pig-latin.js
+++ pig-latin/pig-latin.js
@@ -1,6 +1,11 @@
+const vowels = ['a','e','i','o','u'];
+
 let translator = {
   translate: (word) => {
-    return word;
+    let firstLetter = word.charAt(0);
+    if (vowels.includes(firstLetter)) {
+      return `${word}ay`;
+    };
   }
 }
 
