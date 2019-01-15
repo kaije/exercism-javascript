b8338dbdfda5a4c55c53aa7496290dd0f87426ea
Kelly Farlow <kelly.farlow@gmail.com>
2019-01-15 10:50:52 +1000

    Pythagorean Triplet: Add static where method with maxFactor param - 5 of 7 tests passing

--- pythagorean-triplet/pythagorean-triplet.js
+++ pythagorean-triplet/pythagorean-triplet.js
@@ -13,4 +13,18 @@ export default class Triplet {
   isPythagorean() {
     return this.a ** 2 + this.b ** 2 === this.c ** 2;
   }
+  static where(props) {
+    let maxFactor = props.maxFactor;
+    let triplets = [];
+    for (let a = 1; a <= maxFactor - 2; a++) {
+      for (let b = 2; b <= maxFactor - 1; b++) {
+        for (let c = 3; c <= maxFactor; c++) {
+          if (a < b && b < c && a * a + b * b === c * c) {
+            triplets.push(new Triplet(a, b, c));
+          }
+        }
+      }
+    }
+    return triplets;
+  }
 }
