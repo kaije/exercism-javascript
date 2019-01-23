81285e2097117bcc108c964d8c23575a2bb0e82e
Kelly Farlow <kelly.farlow@gmail.com>
2019-01-23 15:38:00 +1000

    Allergies: Remove commented redundant code

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -9,17 +9,6 @@ const scores = {
   cats: 128
 };
 
-/* const scores = {
-  1: 'eggs',
-  2: 'peanuts',
-  4: 'shellfish',
-  8: 'strawberries',
-  16: 'tomatoes',
-  32: 'chocolate',
-  64: 'pollen',
-  128: 'cats'
-} */
-
 export default class Allergies {
   constructor(score) {
     this.score = score;
