b78492a25f476dc401a20076a18348cde28080b5
Kelly Farlow <kelly.farlow@gmail.com>
2019-02-04 13:57:57 +1000

    House: Continue exercise

--- house/house.js
+++ house/house.js
@@ -1,6 +1,21 @@
+const elements = {
+  1: { noun: "the house that Jack built." },
+  2: { noun: "the malt", postmodifer: "that lay in" }
+};
+
 class House {
   static verse(startVerse) {
-    return ["This is the house that Jack built."];
+    let lyrics = [];
+    let key = startVerse;
+    while (key >= 1) {
+      lyrics.push(
+        key === startVerse
+          ? `This is ${elements[key].noun}`
+          : elements[key].noun
+      );
+      key--;
+    }
+    return lyrics;
   }
 }
 
