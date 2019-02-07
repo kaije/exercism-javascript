d42759ed08e2ba5edee1548d0540c5a9536a594f
Kelly Farlow <kelly.farlow@gmail.com>
2019-02-07 11:58:54 +1000

    House: Add description clause - 12 of 14 tests now passing

--- house/house.js
+++ house/house.js
@@ -3,7 +3,41 @@ const elements = {
   2: { noun: "the malt", postmodifier: "that lay in" },
   3: { noun: "the rat", postmodifier: "that ate" },
   4: { noun: "the cat", postmodifier: "that killed" },
-  5: { noun: "the dog", postmodifier: "that worried" }
+  5: { noun: "the dog", postmodifier: "that worried" },
+  6: {
+    noun: "the cow",
+    description: "with the crumpled horn",
+    postmodifier: "that tossed"
+  },
+  7: {
+    noun: "the maiden",
+    description: "all forlorn",
+    postmodifier: "that milked"
+  },
+  8: {
+    noun: "the man",
+    description: "all tattered and torn",
+    postmodifier: "that kissed"
+  },
+  9: {
+    noun: "the priest",
+    description: "all shaven and shorn",
+    postmodifier: "that married"
+  },
+  10: {
+    noun: "the rooster",
+    description: "that crowed in the morn",
+    postmodifier: "that woke"
+  },
+  11: {
+    noun: "the farmer",
+    description: "sowing his corn",
+    postmodifier: "that kept"
+  },
+  12: {
+    noun: "the horse and the hound and the horn",
+    postmodifier: "that belonged to"
+  }
 };
 
 class House {
@@ -13,6 +47,9 @@ class House {
     let line = "This is";
     while (key > 1) {
       line = `${line} ${elements[key].noun}`;
+      line = elements[key].description
+        ? `${line} ${elements[key].description}`
+        : line;
       lyrics.push(line);
       line = elements[key].postmodifier;
       key--;
