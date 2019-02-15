fc057a4f8a74db07e1d362c301787917dabb2ebc
Kelly Farlow <kelly.farlow@gmail.com>
2019-02-15 14:22:30 +1000

    House: Refactor

--- house/house.js
+++ house/house.js
@@ -1,47 +1,40 @@
 const nounPhrases = {
-  1: { determiner: "the", head: "house", postmodifier: "that Jack built" },
-  2: { determiner: "the", head: "malt", postmodifier: "that lay in" },
-  3: { determiner: "the", head: "rat", postmodifier: "that ate" },
-  4: { determiner: "the", head: "cat", postmodifier: "that killed" },
-  5: { determiner: "the", head: "dog", postmodifier: "that worried" },
+  1: { head: "house", postmodifier: "that Jack built" },
+  2: { head: "malt", postmodifier: "that lay in" },
+  3: { head: "rat", postmodifier: "that ate" },
+  4: { head: "cat", postmodifier: "that killed" },
+  5: { head: "dog", postmodifier: "that worried" },
   6: {
-    determiner: "the",
     head: "cow",
     description: "with the crumpled horn",
     postmodifier: "that tossed"
   },
   7: {
-    determiner: "the",
     head: "maiden",
     description: "all forlorn",
     postmodifier: "that milked"
   },
   8: {
-    determiner: "the",
     head: "man",
     description: "all tattered and torn",
     postmodifier: "that kissed"
   },
   9: {
-    determiner: "the",
     head: "priest",
     description: "all shaven and shorn",
     postmodifier: "that married"
   },
   10: {
-    determiner: "the",
     head: "rooster",
     description: "that crowed in the morn",
     postmodifier: "that woke"
   },
   11: {
-    determiner: "the",
     head: "farmer",
     description: "sowing his corn",
     postmodifier: "that kept"
   },
   12: {
-    determiner: "the",
     head: "horse and the hound and the horn",
     postmodifier: "that belonged to"
   }
@@ -49,21 +42,19 @@ const nounPhrases = {
 
 class House {
   static verse(startVerse) {
-    let lyrics = [];
     let key = startVerse;
+    let lyrics = [];
     let line = "This is";
     while (key > 1) {
-      let phrase = nounPhrases[key];
-      line = `${line} ${phrase.determiner} ${phrase.head}`;
-      line = phrase.description ? `${line} ${phrase.description}` : line;
+      let phrase = nounPhrases[key--];
+      line = `${line} the ${phrase.head}${
+        phrase.description ? " " + phrase.description : ""
+      }`;
       lyrics.push(line);
       line = phrase.postmodifier;
-      key--;
     }
     lyrics.push(
-      `${line} ${nounPhrases[key].determiner} ${nounPhrases[key].head} ${
-        nounPhrases[key].postmodifier
-      }.`
+      `${line} the ${nounPhrases[key].head} ${nounPhrases[key].postmodifier}.`
     );
     return lyrics;
   }
