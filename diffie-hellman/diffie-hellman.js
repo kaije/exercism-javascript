8e49d5aab9c662c1f5b2616d79e4c943d7405716
Kelly Farlow <kelly.farlow@gmail.com>
2019-02-20 17:10:00 +1000

    Diffie-Hellman: Add check for primes - 2 or 10 tests passing

--- diffie-hellman/diffie-hellman.js
+++ diffie-hellman/diffie-hellman.js
@@ -3,6 +3,18 @@ class DiffieHellman {
     if (p < 1 || g > 100) {
       throw new Error('Constructor arguments out of range')
     }
+    if (!this.isPrime(p) || !this.isPrime(g)) {
+      throw new Error('Constructor arguments must be prime')
+    }
+  }
+  isPrime(number) {
+    var start = 2;
+    while (start <= Math.sqrt(number)) {
+      if (number % start++ < 1) {
+        return false;
+      }
+    }
+    return number > 1;
   }
 }
 
