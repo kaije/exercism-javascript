6af6ab7cd0f7eb9a1777c6675a95aec9fe58ffdf
Kelly Farlow <kelly.farlow@gmail.com>
2019-03-05 09:57:48 +1000

    Twelve Days: Extend solution - 2 of 12 tests now passing

--- twelve-days/twelve-days.js
+++ twelve-days/twelve-days.js
@@ -1,9 +1,23 @@
 class TwelveDays {
-  constructor() {}
+  constructor() {
+    this.gifts = {
+      1: { day: "first", gift: "a Partridge in a Pear Tree" },
+      2: { day: "second", gift: "two Turtle Doves" },
+      3: { day: "third", gift: "three French Hens" }
+    };
+  }
   verse(id) {
-    if (id === 1) {
-      return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n";
+    return `On the ${
+      this.gifts[id].day
+    } day of Christmas my true love gave to me: ${this.getList(id)}.\n`;
+  }
+  getList(id) {
+    let list = "";
+    for (let i = id; i >= 1; i--) {
+      list = list.length > 0 ? `${list}, ` : list;
+      list = `${list}${i === 1 && id > 1 ? `and ` : ``}${this.gifts[i].gift}`;
     }
+    return list;
   }
 }
 
