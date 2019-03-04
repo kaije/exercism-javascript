58e3f5f1aa45cb66526e0e2f3a5becf827fe39b7
Kelly Farlow <kelly.farlow@gmail.com>
2019-03-04 16:10:38 +1000

    Twelve Days: Start exercise - first test passing

---
+++ twelve-days/twelve-days.js
@@ -0,0 +1,10 @@
+class TwelveDays {
+  constructor() {}
+  verse(id) {
+    if (id === 1) {
+      return "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n";
+    }
+  }
+}
+
+export { TwelveDays };
